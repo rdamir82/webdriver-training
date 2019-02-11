0db05e78842dd3e6adaf9b5c006cfec4405a4fa9
rdamir82 <rdamir82@gmail.com>
2019-02-11 17:42:46 +0100

    Small changes

--- src/test/java/com/webdrivertraining/tests/CheckBoxes.java
+++ src/test/java/com/webdrivertraining/tests/CheckBoxes.java
@@ -1,6 +1,5 @@
 package com.webdrivertraining.tests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -11,45 +10,37 @@ import org.openqa.selenium.WebElement;
 import com.webdrivertraining.testbase.TestBase;
 
 public class CheckBoxes extends TestBase {
-	
+
 	String baseUrl = "http://the-internet.herokuapp.com/checkboxes";
 	private By checkbox1 = By.cssSelector("#checkboxes > input:nth-child(1)");
 	private By checkbox2 = By.cssSelector("#checkboxes > input:nth-child(3)");
-	
-	
+
 	@Test
-	 public void checboxCheck() {
-		
+	public void checboxCheck() {
+
 		getDriver().get(baseUrl);
-		
+
 		WebElement checkBox1 = getDriver().findElement(checkbox1);
 		WebElement checkBox2 = getDriver().findElement(checkbox2);
-			
-		assertFalse(checkBox1.isSelected()); 
+
+		assertFalse(checkBox1.isSelected());
 		assertTrue(checkBox2.isSelected());
-		
-		
-	//	System.out.println(checkIfSelected(checkBox1));
-		
-	//	System.out.println(checkIfSelected(checkBox2));		
-		
+
+		// System.out.println(checkIfSelected(checkBox1));
+
+		// System.out.println(checkIfSelected(checkBox2));
+
 	}
 	/*
-	private String checkIfSelected(WebElement webElement) {
-		if (webElement.isSelected()) {
-			
-			return (webElement.getAttribute("outerHTML") + "Is selected");
-		} else {
-			return (webElement.getAttribute("outerHTML") + "Is NOT selected");
-		}
-		
-	}
-	*/
-
-	
-	
+	 * private String checkIfSelected(WebElement webElement) { if
+	 * (webElement.isSelected()) {
+	 * 
+	 * return (webElement.getAttribute("outerHTML") + "Is selected"); } else {
+	 * return (webElement.getAttribute("outerHTML") + "Is NOT selected"); }
+	 * 
+	 * }
+	 */
 
 }
-
 
 // #checkboxes > input:nth-child(1)
