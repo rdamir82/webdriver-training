339bb54ff675adcf1deacdee73da4a62f4b3ffb4
rdamir82 <rdamir82@gmail.com>
2019-02-14 13:56:33 +0100

    brokenImages refactoring test drag and drop

--- src/test/java/com/webdrivertraining/tests/BrokenImages.java
+++ src/test/java/com/webdrivertraining/tests/BrokenImages.java
@@ -3,34 +3,39 @@ package com.webdrivertraining.tests;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import com.webdrivertraining.testbase.TestBase;
 
 public class BrokenImages extends TestBase {
 	
-	String baseUrl = "http://the-internet.herokuapp.com/broken_images";
-	String img1selector = ".example > img:nth-child(2)"; 
-	String img2selector = ".example > img:nth-child(3)";
-	String img3selector = ".example > img:nth-child(4)";
-	
-	
-	// ArrayList<String> imgSelectors = new ArrayList<String>();
-	
-	
-	
+	final String baseUrl = "http://the-internet.herokuapp.com/broken_images";
+	String res = "";
+	boolean forTest = true;	
 	@Test
 	 public void brokenImages() {
 		
 		getDriver().get(baseUrl);
 		
-		assertTrue(!getDriver().findElement(By.cssSelector(img3selector)).getAttribute("naturalWidth").equals("0"));
+		List<WebElement> imgsForTest = getDriver().findElements(By.tagName("img"));
 		
-
-		
-		
+		// if find broken img inform the location of that img and set false for testCase
+		for (WebElement element : imgsForTest) {
+			
+			res = element.getAttribute("naturalWidth");
+			
+			if (res.equals("0")) {
+				System.out.println("IMG BROKEN ! @ location: " + element.getLocation());
+				forTest = false;
+			}					
+		}		
+		// test will pass if all img elements is appear on page!
+		assertTrue(forTest);
+				
 	}
 
 }
